0ceddf954036b4cec4c117277e7de530f17267df
kwon_si <kwonsoonil8568@gmail.com>
2023-02-20 19:16:40 +0900

    첫번째 커밋 생성

---
+++ practice/GitPracticeRun.java
@@ -0,0 +1,11 @@
+package edu.kh.git.practice;
+
+public class GitPracticeRun {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하새우");
+		System.out.println("Git연습용파일입니당");
+		
+	}
+
+}
